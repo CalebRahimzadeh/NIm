5e9bee728aab4d3d1dfc3ad2ca2913cbd20c6ba3
Caleb Rahimzadeh <calebrahimzadeh@hotmail.com>
2016-09-26 11:48:13 -0600

    Basic class skeleton

--- Nim/Nim/NimController.cs
+++ Nim/Nim/NimController.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace Nim
 {
-    class Program
+    class NimController
     {
-        static void Main(string[] args)
-        {
-
-        }
     }
 }
---
+++ Nim/Nim/State.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nim
+{
+    class State
+    {
+        public int RowOneValues { get; set; }
+        public int RowTwoValues { get; set; }
+        public int RowThreeValues { get; set; }
+
+        public int Average { get; set; }
+    }
+}
