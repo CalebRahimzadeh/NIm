9bd231ba5e53b30cc919fedea19ca623cdcdbbcf
D-Blake <dblake.cccmc@gmail.com>
2016-09-27 15:24:41 -0600

    Conflicts Changed

--- Nim/Nim/GameEngine.cs
+++ Nim/Nim/GameEngine.cs
@@ -1,89 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Nim
-{
-    class GameEngine
-    {
-        private const int MAX_ROWS = 3;
-        private int[] _board;
-        private bool isTurn;
-        //create board
-        public GameEngine()
-        {
-            //Random Turn
-            isTurn = StartingTurn();
-            // Construct rows.
-            _board = new int[MAX_ROWS] { 3, 5, 7 };
-            if (!AI.StateTree.ContainsKey(_board))
-            {
-                State defaultState = new State(_board) { Average = 0 };
-                AI.StateTree.Add(_board, defaultState);
-            }
-        }
-
-        public void Run()
-        {
-            //play game     
-            State currentState = new State(_board);
-        }
-
-        public void PlayComputerVsPlayer()
-        {
-            TakeTurn();
-        }
-
-        public void PlayComputerVsComputer()
-        {
-            TakeTurn();
-        }
-
-        public void PlayerVsPlayer()
-        {
-            TakeTurn();
-        }
-
-        public void RemovePieces(int targetRow, int removeAmt)
-        {
-
-        }
-        public bool StartingTurn()
-        {
-            Random rand = new Random();
-            bool boolReturn;
-            if (rand.Next(2) == 0)
-            {
-                boolReturn = false;
-            }
-            else
-            {
-                boolReturn = true;
-            }
-            return boolReturn;
-        }
-
-        public void TakeTurn()
-        {
-            if (isTurn)
-            {
-                RemovePieces(UI.PromptRow(), UI.PromptRemoval());
-            }
-            else if (!isTurn)
-            {
-                RemovePieces(UI.PromptRow(), UI.PromptRemoval());
-            }
-            else
-            {
-                Console.WriteLine("ERROR: TakeTurn Bool");
-            }
-        }
-    }
-
-}
-
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -97,7 +12,6 @@ namespace Nim
         private const int MAX_ROWS = 3;
         private int[] _board;
         private bool isTurn;
-        private 
         //create board
         public GameEngine()
         {
@@ -125,7 +39,6 @@ namespace Nim
 
         public void PlayComputerVsComputer()
         {
-            AI 
             TakeTurn();
         }
 
@@ -141,7 +54,7 @@ namespace Nim
         public bool StartingTurn()
         {
             Random rand = new Random();
-            bool boolReturn;            bool boolReturn = false;
+            bool boolReturn = false;
             if(rand.Next(2) == 0)
             {
                 return boolReturn;
@@ -156,11 +69,11 @@ namespace Nim
         {
             if (isTurn)
             {
-
+                RemovePieces(UI.PromptRow(), UI.PromptRemoval());
             }
             else if (isTurn)
             {
-
+                RemovePieces(UI.PromptRow(), UI.PromptRemoval());
             }
         }
     }
